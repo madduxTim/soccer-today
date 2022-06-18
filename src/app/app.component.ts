9b5a88857ef6c5f069b80495af565b8ad2ad1d73
tim <timothymaddux@gmail.com>
2022-06-18 07:33:54 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'soccer-today';
+  title = 'soccer-today';  
 }
+
+console.log("init");
